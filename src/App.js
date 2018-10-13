18ec48cdc44a9e482a61827be6c7218f10fdecd1
장지민 <Jimin@jangjimin-ui-MacBook-Air.local>
2018-10-14 04:57:39 +0900

    basic form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+
+import Main from './container/Main';
+import Second from './container/Second';
+import Form from './room/Form';
+import List from './room/List';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+      <Router>
+      <div>
+        <Route exact path="/" component ={Main}/>
+        <Route path="/second" component={Second}/>
+        <Route path="/form" component={Form}/>
+        <Route path="/list" component={List}/>
+
       </div>
+      </Router>
     );
   }
 }
---
+++ src/component/Menu.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Menu =()=>(
+    <div className="menu">
+    menu
+    </div>
+)
+
+export default Menu;
